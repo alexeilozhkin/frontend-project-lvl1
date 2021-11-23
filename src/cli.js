6f8216ba9f53a84a3997dab782c38db5734554a7
alexeilozhkin <alexeilozhkin983@gmail.com>
2021-11-24 00:34:07 +0400

    add linter-check.yml & modified Makefile, brain-games.js, cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
-export function getUserName() {
-    const userName = readlineSync.question('May I have your name?');
-    console.log('Hello, ' + userName + '!');
-};
+
+function getUserName() {
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+}
+export default getUserName;
