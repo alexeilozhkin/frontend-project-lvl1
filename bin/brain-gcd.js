642c18758381babea2d012c6fa22fc677633217f
alexeilozhkin <alexeilozhkin983@gmail.com>
2021-12-21 01:32:53 +0400

    fixed brain-games

--- bin/brain-gcd.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import getUserCommunication from '../src/index.js';
-import { ruleMessage, getGcdQuestionAndAnswer } from '../src/games/gcd.js';
-
-getUserCommunication(ruleMessage, getGcdQuestionAndAnswer);
