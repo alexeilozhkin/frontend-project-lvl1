d01cb52fbd7da6800fd1d790e5e1ef3beb91fc88
alexeilozhkin <alexeilozhkin983@gmail.com>
2022-02-09 22:04:36 +0400

    modified progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,15 +2,17 @@ import getRandomNumber from '../randomNumber.js';
 
 const ruleMessage = 'What number is missing in the progression?';
 const getProgressionQuestionAndAnswer = () => {
-  let progressionNumber = getRandomNumber(10);
-  const progressionStep = getRandomNumber(15);
-  const progressionLength = 10;
-  const progression = [];
-  for (let i = 0; i < progressionLength; i += 1) {
-    progression.push(progressionNumber);
-    progressionNumber += progressionStep;
-  }
-  const randomIndex = getRandomNumber(progressionLength);
+  const setProgression = (firstNumber, progressionStep, progressionLength) => {
+    const progressionArray = [firstNumber];
+    let currentNumber = firstNumber;
+    for (let i = 0; i < progressionLength - 1; i += 1) {
+      currentNumber += progressionStep;
+      progressionArray.push(currentNumber);
+    }
+    return progressionArray;
+  };
+  const progression = setProgression(getRandomNumber(10), getRandomNumber(15), 15);
+  const randomIndex = getRandomNumber(progression.length);
   const correctAnswer = progression[randomIndex];
   progression[randomIndex] = '..';
   const question = progression.join(' ');
